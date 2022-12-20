6f130316b67b4c0f21f31ad9e2d4104bbbacec61
nagilla748 <venkatesh.nagilla@outlook.com>
2022-12-20 12:34:45 +0530

    added python3.11 features

---
+++ part2/list.py
@@ -0,0 +1,14 @@
+# List 
+# list() 
+
+d = {1: "1", 2: "2", 3: "3"}
+print(list(d))
+
+s = set([1, 2, 3, 4, 5])
+print(list(s))
+
+string = "Hello World"
+print(list(string))
+
+tup = (1, 2, 3, 4, 5)
+print(list(tup))
